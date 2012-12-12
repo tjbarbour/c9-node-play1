38ed5e6635de684dd54789008a1230eacfe036a9
T.J. Barbour <tjbarbour@gmail.com>
2012-12-12 03:13:20 -0500

    initial version, simple html over http server

---
+++ hello.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(req, res) {
+    if(req.method == "GET"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile( "index.html","utf8", function(err, data) {
+            var template = data.toString("utf",0,data.length);
+            template = template.replace("$TOKEN$", new Date().toString());
+            res.write(template);
+            res.end();
+        });
+    }
+}).listen(process.env.PORT);
